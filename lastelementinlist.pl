feb47db808422a43c1c4b964de61b53a1c609505
smilinghulkress <smilinghulkress@users.noreply.github.com>
2017-10-06 03:58:34 -0400

    To find the last element in the list.

---
+++ lastelementinlist.pl
@@ -0,0 +1,16 @@
+findlast([],0).
+findlast([H|T],X):-
+	length([H|T],Y),
+	(Y =:= 1
+		->
+		X=H
+		;findlast(T,X)
+	).
+
+length([],0).
+length([H|T],X):-
+	length(T, Prev),
+	X is Prev +1.
+	
+
+	
